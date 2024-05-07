60e906a88df265121867f8feedcc3220f7ed8c27
Maaz_ahmed <maazwaqar076@gmail.com>
2024-05-07 06:29:49 +0500

    npx tool work done

--- index.ts
+++ index.ts
@@ -2,67 +2,90 @@
 
 import inquirer from 'inquirer';
 import chalk from 'chalk';
-
-
-//greeting user with a welcome message
-async function greeting() {
-    console.log(chalk.green('\nWelcome to the Number Guessing Game!'));
-
-        console.log(chalk.bold.hex('#DEADED').bold('Develop By Maaz Ahmed'));
-    
+  
+// welcome message with chalkANimation
+async function welcomeMessage() {
+    console.log(chalk.bold.hex('#DEADED').bold('Develop By Maaz Ahmed'));
 }
- greeting();
 
-// random number guesing fnction
-async function guessNumber() {
-    // await greeting();
-    console.log(chalk.blackBright.bgCyan("\nI'm thinking of a number between 1 to 10. Can you guess it?"))
-    
-    //gnerating random number between 1 t0 10
-    const secretNumber = Math.floor(Math.random() * 10)
-    
-    let attempt = 0
-    
-    let answers;
-    do{
-        answers = await inquirer
-        .prompt([
-            {
-                type: "number",
-                name: "userGuess",
-                message: "enter your number"
-            }
-        ]);
-        if
-        (answers.userGuess <1 || answers.userGuess > 10){
-            console.log(chalk.red.bgWhite("Please enter a number between 1 and 10!"));
-        }
-    }while
-        (answers.userGuess < 1 || answers.userGuess > 10)
-        attempt++  
-    if(answers.userGuess === secretNumber){
-        console.log(`WOooo! you guessed the ${secretNumber} correctly in ${attempt} attempts`);
+await welcomeMessage()
+
+
+async function userAuth() {
+   
+    let randomId = Math.floor(Math.random() * 4135)
+    console.log(`Your Id is ${randomId}`)
+    let randomPin = Math.floor(Math.random() * 6028)
+    console.log(`Your pin is ${randomPin}`)
+    let isAuth = false;
+    while (!isAuth) {
+       const answers = await inquirer 
+    .prompt([
+        {
+            type: 'input',
+            name : 'userId',
+            message: 'please enter you id',
+        },
+        {
+            type: 'number',
+            name : 'userPin',
+            message: 'please enter you pin'
+        },
+    ]);
+    const parsedUserID = parseInt(answers.userId)
+    const parsedUserPin = parseInt(answers.userPin)
+
+    if 
+    (parsedUserID === randomId  && parsedUserPin === randomPin)
+    {
+        console.log(chalk.green("Authentication successful! Welcome to the ATM."));
+        isAuth = true;
     }
     else{
-        console.log(`Oooops! you guessed worng number the correct number is ${secretNumber}`);
+        console.log(chalk.red('Invalid user ID or PIN. Please try again.'))
     }
+    }
+    
+    
+}
+
+
+
+async function atmFun() {
+   await userAuth();
+    let replys = await inquirer .prompt([
+        {
+           type: 'list',
+            name : 'accType',
+            choices:["Saving","current"],
+           message: 'Select your account type'
+       },
+       {
+           type: 'list',
+           name : 'transactionType',
+           choices:["FastCash","withdrawal"],
+           message: 'Select your transaction type',
+           when: replys => replys.accType,
+       
+       },
+       {
+           type: 'list',
+           name : 'amount',
+           choices:["1000","5000", "10000"],
+           message: 'Select your amount',
+           when : replys => replys.transactionType === "FastCash",
+       },
+       {
+           type: 'number',
+           name : 'amount',
+           message: 'Select your amount',
+           when : replys => replys.transactionType === "withdrawal",
+       },
+   ]);
+   if (replys.transactionType === "withdrawal" || "FastCash") {
+       console.log(`You have withdrawn ${replys.amount} from your account`)
+   }
 
 }
 
-// function to re start the game is user wanted
-
-async function tryAgain() {
-    do{
-        await guessNumber();
-        var askAgain = await inquirer .prompt([
-            {
-                type: "input",
-                name: "userChoice",
-                message: "Do you want to try again? Press 'y' to guess again or 'n' to stop"
-            }
-        ]);
-    }
-    while(askAgain.userChoice === "y" || askAgain.userChoice === "Y" || askAgain.userChoice === "yes" || askAgain.userChoice === "YES")
-}
-
-tryAgain();
+atmFun();
